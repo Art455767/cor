77c034c828d0baf047bb473ce49cd4b86def503b
Digma <opt.artem@bk.ru>
2024-12-17 18:30:07 +0300

    one commit

---
+++ src/main/kotlin/ru/netology/coroutines/dto/Post.kt
@@ -0,0 +1,11 @@
+package ru.netology.coroutines.dto
+
+data class Post<Author>(
+    val id: Long,
+    val authorId: Long,
+    var author: Author? = null,
+    val content: String,
+    val published: Long,
+    val likedByMe: Boolean,
+    val likes: Int = 0
+)
